eb59ccfe0c39f696bb0568c0725781ac9f83a6ca
Javier Gamarra <nhpatt@gmail.com>
2015-09-06 23:59:15 +0200

    example fragment implementation

---
+++ app/src/main/java/com/nhpatt/asde/mvp/fragments/ItemDetailFragment.java
@@ -0,0 +1,39 @@
+package com.nhpatt.asde.mvp.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nhpatt.asde.R;
+
+
+public class ItemDetailFragment extends Fragment {
+
+    public static final String ARG_ITEM_ID = "item_id";
+
+    public ItemDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        if (getArguments().containsKey(ARG_ITEM_ID)) {
+//            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+//        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
+
+//        if (mItem != null) {
+//            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.content);
+//        }
+
+        return rootView;
+    }
+}
