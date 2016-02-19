85fa99938db05ccc6f075d3df1af7b5ba245a869
Javier Gamarra <nhpatt@gmail.com>
2016-02-19 17:59:34 +0100

    example with rxjava

--- app/src/main/java/com/nhpatt/asde/async/interactors/SearchCatalogListInteractor.java
+++ app/src/main/java/com/nhpatt/asde/async/interactors/SearchCatalogListInteractor.java
@@ -1,35 +1,35 @@
 package com.nhpatt.asde.async.interactors;
 
-import com.nhpatt.asde.async.EventBusUtil;
 import com.nhpatt.asde.async.services.GitHubService;
 import com.nhpatt.asde.models.Commits;
 
 import java.io.IOException;
 import java.util.List;
 
-import retrofit2.Call;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * @author Hugo Nebreda
  */
-public class SearchCatalogListInteractor extends AbstractInteractor {
-    @Override
-    public void runOnBackground() throws IOException {
+public class SearchCatalogListInteractor extends AbstractRxInteractor {
+
+    public Observable<List<Commits>> runOnRx() {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.github.com")
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
 
         GitHubService github = retrofit.create(GitHubService.class);
 
-        Call<List<Commits>> call = github.commits("nhpatt", "Catalogo-de-Actividades-ASDE");
-
-        List<Commits> commits = call.execute().body();
-
-        EventBusUtil.post(commits);
-
+        return github.commits("nhpatt", "Catalogo-de-Actividades-ASDE")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
     }
 }
