69dcf31b8b423636dc54b0893ce98419b9f4ac58
Javier Gamarra <nhpatt@gmail.com>
2016-02-27 16:45:52 +0100

    use recycler view

---
+++ app/src/main/java/com/nhpatt/asde/mvp/activities/EventListener.java
@@ -0,0 +1,11 @@
+package com.nhpatt.asde.mvp.activities;
+
+import com.nhpatt.asde.models.Event;
+
+/**
+ * @author Javier Gamarra
+ */
+public interface EventListener {
+
+    void click(Event event);
+}
