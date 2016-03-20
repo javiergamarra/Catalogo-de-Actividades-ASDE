0f453f058922c91a4fe9a6b44bf4df567dd01c2b
Javier Gamarra <nhpatt@gmail.com>
2016-03-21 00:25:50 +0100

    simplify interactors

--- app/src/main/java/com/nhpatt/asde/async/interactors/AbstractInteractor.java
+++ app/src/main/java/com/nhpatt/asde/async/interactors/AbstractInteractor.java
@@ -3,33 +3,14 @@ package com.nhpatt.asde.async.interactors;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 
 import com.nhpatt.asde.async.RetrofitAPI;
 
 import retrofit2.Retrofit;
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 
 public abstract class AbstractInteractor {
-
-    @NonNull
-    @CheckResult
-    protected static <T> Observable.Transformer<T, T> background() {
-        return obs -> obs
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .cache();
-    }
-
-    public abstract Observable run(Object... arguments);
-
-    public Observable runInBackground(Object... arguments) {
-        return run(arguments).compose(background());
-    }
 
     @NonNull
     protected Retrofit getApiary() {
