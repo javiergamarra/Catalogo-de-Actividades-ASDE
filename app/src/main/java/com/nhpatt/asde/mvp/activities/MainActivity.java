d00bdaed27ae724cf9c9b37dc699aed4e16db62a
Javier Gamarra <nhpatt@gmail.com>
2016-05-15 17:12:16 +0200

    load first fragment

--- app/src/main/java/com/nhpatt/asde/mvp/activities/MainActivity.java
+++ app/src/main/java/com/nhpatt/asde/mvp/activities/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        onNavigationItemSelected(navigationView.getMenu().getItem(0));
     }
 
     @Override
